1660d39744c1e92bc30ff7d41ab7dc661704b688
opmvpc <thibsix@outlook.be>
2023-09-24 22:02:13 +0200

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'personne/index.html'),
+        personne: resolve(__dirname, 'personne/index.html'),
       },
     },
   },
