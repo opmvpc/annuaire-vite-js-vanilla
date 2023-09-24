d72f40807c0e51d62b86568700c6c316727f5c52
opmvpc <thibsix@outlook.be>
2023-09-24 17:59:44 +0200

    2 files modified, 1 added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as bootstrap from 'bootstrap';
 import './style.scss';
 import { data } from './data';
+import { nav } from './nav';
 
 const listePersonnes = () => {
   let html = '';
@@ -21,21 +22,7 @@ const listePersonnes = () => {
 
 document.querySelector('#app').innerHTML = `
   <main>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Annuaire</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">Accueil</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    ${nav}
 
     <div class="container-fluid mt-4">
       <div class="d-flex gap-3 flex-wrap">
--- personne.js
+++ personne.js
@@ -1,17 +1,19 @@
 import * as bootstrap from 'bootstrap';
 import './style.scss';
 import { data } from './data';
+import { nav } from './nav';
 
 const detailsPersonne = () => {
+  // récupération des paramètres GET de l'url
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const personneId = urlParams.get('id');
   console.log(personneId);
-  console.log(data);
 
   const personne = data.find((personne) => {
     return personne.id === Number.parseInt(personneId);
   });
+  console.log(personne);
 
   return `
       <div class="card">
@@ -28,21 +30,7 @@ const detailsPersonne = () => {
 
 document.querySelector('#app').innerHTML = `
   <main>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Annuaire</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">Accueil</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    ${nav}
 
     <div class="container-fluid mt-4">
       ${detailsPersonne()}
